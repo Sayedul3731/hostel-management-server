085b2138f3ac28849182d418c418133a38a7e779
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-11-26 08:47:48 +0600

    identify user role

--- src/routes/meals/index.js
+++ src/routes/meals/index.js
@@ -5,6 +5,7 @@ const meal = require('../../models/Meals');
 const user = require('../../models/Users')
 
 router.get('/meals', findAll);
+
 router.get('/users', async(req, res) => {
     const result = await user.find()
     res.send(result)
@@ -15,10 +16,28 @@ router.get('/meals/:id', async (req, res) => {
     const result = await meal.find({ _id: id })
     res.send(result)
 })
+router.get('/users/admin/:email', async (req, res) => {
+    console.log('admin email', req.params.email);
+
+    const adminUser = await user.findOne({ email: req.params.email});
+    let admin = false;
+    if(adminUser){
+        admin= adminUser?.role === 'Admin'
+    }
+    res.send({admin})
+})
 
 router.post('/users', async (req, res) => {
     const newUser = new user(req.body);
     await newUser.save()
+})
+router.patch('/users/admin/:id', async (req, res) => {
+    const result = await user.updateOne({_id: req.params.id}, {
+        $set: {
+            role: 'Admin'
+        }
+    })
+    res.send(result)
 })
 
 module.exports = router
